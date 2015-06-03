a2b8521b8e33ab38a3b36cdc8b7820d363f1e4d3
Davide Polonio <poloniodavide@gmail.com>
2015-06-03 22:56:58 +0200

    Primo test funzionante sito.
    -Prima implementazione di prova con integrazione del foglio di stile.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+
+<html>
+<?php
+include("include/site/head.html");
+//echo "<html><body><p>Prova</p></body></html>";
+?>
+    <body>
+    <?php include("include/site/menu.html"); ?>
+    </body>
+
+</html>
