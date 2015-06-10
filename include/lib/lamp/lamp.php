2bc28d1e362d509177a91c7690f20d2195cd9413
Davide Polonio <poloniodavide@gmail.com>
2015-06-10 22:21:33 +0200

    Spostamento lamp.php sotto cartella per rendere il progetto più ordinato

---
+++ include/lib/lamp/lamp.php
@@ -0,0 +1,22 @@
+<?php
+
+function getLampshade(){
+    //query che mi selezionerà tutti i paralumi
+    ?>
+    <option value="321432">Paralume 1</option>
+    <option value="854852">Paralume 2</option>
+    <option value="756488">Paralume 3</option>
+
+<?php
+}
+
+function getVase(){
+ //query che mi darà tutti i vasi disponibili per essere abbinati con i paralumi
+    ?>
+    <option value="156156">Vaso 1</option>
+    <option value="756415">Vaso 2</option>
+    <option value="476152">Vaso 3</option>
+
+<?php
+}
+?>
