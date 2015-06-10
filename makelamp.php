15041d28fac0271d818a61eb6a0a5fd6d8484d00
Davide Polonio <poloniodavide@gmail.com>
2015-06-10 22:23:09 +0200

    Integrazione con lib/lamp
    -Aggiunta dell'inclusione di lamp/lamp.php
    -Aggiunto nel form l'utilizzo di lamp/order.php

--- makelamp.php
+++ makelamp.php
@@ -5,6 +5,7 @@
     <?php
         //Creo l'header in cui includo il foglio di stile e setto la pagina del titolo
         include("include/page.php");
+        include_once("include/lib/lamp/lamp.php");
         getHeader("Crea la tua lampada");
     ?>
     <body>
@@ -17,10 +18,16 @@
             <p>Seleziona il paralume e il vaso che preferisci, e noi creeremo la tua lampada personalizzata.</p>
             <div id="makelamp">
 
-                <form id="makelamp" action="include/lib/lamp.php">
+                <form id="makelamp" action="include/lib/lamp/order.php">
                     <select>
-                        <option value="321432">Nome_Paralume</option>
+                        <?php getLampshade(); ?>
                     </select>
+
+                    <select>
+                        <?php getVase(); ?>
+                    </select>
+
+                    <input type="submit" value="Submit">
                 </form>
 
             </div>
