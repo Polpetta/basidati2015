a833b906ff548e4241c87b76e5f299bdcad1decb
Davide Polonio <poloniodavide@gmail.com>
2015-06-10 22:22:04 +0200

    Aggiunto futuro order che si occuperà di gestire l'ordine di una lampada

---
+++ include/lib/lamp/order.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+In questo file verrà gestita la richiesta di un ordine di una lampada, quindi si prenderanno gli argomenti passati per post, si eseguirà
+la query per scalare un paralume e un vaso e si creerà uno scontrino sul database.
+*/
+
+?>
