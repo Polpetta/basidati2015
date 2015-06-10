b021497627ecbc154d55fd34e2dff484c78a16c6
Davide Polonio <poloniodavide@gmail.com>
2015-06-10 11:12:49 +0200

    Piccola descrizione aggiunta nella homepage

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
             getMenu()
         ?>
        <div id="main">
+           <h1>Benvenuto</h1>
+           <p>
+               In questo negozio si troveranno articoli per la casa, comprese tazze fatte a mano, vasi, abat jour e argenteria. <br>
+               Per cominciare, scorri il mouse sopra il tasto del men&ugrave;.
+           </p>
          <p>prova</p>
         <p>prova</p>
         <p>prova</p>
