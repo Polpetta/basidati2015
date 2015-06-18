fe0984e9b99307e06240eb817578d02ac626b0d0
Davide Polonio <poloniodavide@gmail.com>
2015-06-18 14:42:50 +0200

    File di creazione delle tabelle completato. È necessario il comando "SET FOREIGN_KEY_CHECKS=0;" perchè vada.

--- sql/createtable.sql
+++ sql/createtable.sql
@@ -2,6 +2,11 @@ drop table if exists Categoria;
 drop table if exists Sconto;
 drop table if exists Scaglioni;
 drop table if exists Dipendente;
+drop table if exists Prodotto;
+drop table if exists Scontrino;
+drop table if exists Iscritto;
+drop table if exists Fornitore;
+drop table if exists Fattura;
 
 CREATE TABLE Categoria(
 NomeCategoria	CHAR(20) PRIMARY KEY
@@ -34,9 +39,69 @@ CodFiscale	CHAR(10) NOT NULL,
 Telefono	CHAR(10) NOT NULL,
 Mail		CHAR(20),
 DataInizio	DATE NOT NULL,
+Indirizzo	CHAR(50),
 Categoria	CHAR(20),
 
 PRIMARY KEY (CodDipendente),
 FOREIGN KEY (Categoria) REFERENCES Categoria (NomeCategoria)
 ) ENGINE=InnoDB;
 
+CREATE TABLE Prodotto(
+CodProdotto  INT AUTO_INCREMENT,
+Nome	     CHAR(50) NOT NULL,
+Descrizione  TEXT,
+Quantita     SMALLINT DEFAULT 0 NOT NULL,
+Costo	     INT DEFAULT 0 NOT NULL,
+PercentualeIVA	 INT(2) DEFAULT 0 NOT NULL,
+Foto		 MEDIUMBLOB,
+Categoria	 CHAR(20),
+
+PRIMARY KEY (CodProdotto),
+FOREIGN KEY (Categoria) REFERENCES Categoria(NomeCategoria)
+) ENGINE=InnoDB;
+
+CREATE TABLE Scontrino(
+Prodotto     INT,
+Data	     DATE,
+CodScontrino INT,
+Quantita     SMALLINT NOT NULL,
+SubTotale    INT NOT NULL,
+Iscritto     INT,
+
+PRIMARY KEY (Prodotto,Data,CodScontrino),
+FOREIGN KEY (Iscritto) REFERENCES Iscritto(CodIscritto)
+) ENGINE=InnoDB;
+
+CREATE TABLE Iscritto(
+CodIscritto  INT AUTO_INCREMENT,
+Nome	     CHAR(20) NOT NULL,
+Cognome	     CHAR(20) NOT NULL,
+Fax	     CHAR(20),
+Telefono     CHAR(20),
+Mail	     CHAR(20),
+Indirizzo    CHAR(50),
+
+PRIMARY KEY (CodIscritto)
+) ENGINE=InnoDB;
+
+CREATE TABLE Fornitore(
+Nome   CHAR(50),
+Fax    CHAR(10),
+Telefono	CHAR(10) NOT NULL,
+Mail		CHAR(10) NOT NULL,
+Indirizzo	CHAR(50) NOT NULL,
+
+PRIMARY KEY (Nome)
+) ENGINE=InnoDB;
+
+CREATE TABLE Fattura(
+Prodotto     INT,
+CodFattura   INT,
+Data	     DATE,
+Quantita     SMALLINT,
+Fornitore    CHAR(50),
+
+PRIMARY KEY (Prodotto,CodFattura),
+FOREIGN KEY (Prodotto) REFERENCES Prodotto (CodProdotto),
+FOREIGN KEY (Fornitore) REFERENCES Fornitore (Nome)
+) ENGINE = InnoDB;
