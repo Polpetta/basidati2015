71ba09eca54aae1493f6fd31a88e44a7a04c679c
Davide Polonio <poloniodavide@gmail.com>
2015-06-19 19:51:21 +0200

    Importanti modifiche alla creazione delle tabelle. -Ora le tabelle con relazione di tipo (N,N) che hanno tra la loro chiave una chiave esterna sono state modificate inserendo un attributo "Id INT AUTO_INCREMENT" come PRIMARY KEY, semplificando il processo di creazione del database. -Da fixare gli ON DELETE CASCADE nelle FOREIGN KEY.

--- sql/createtable.sql
+++ sql/createtable.sql
@@ -7,6 +7,8 @@ drop table if exists Scontrino;
 drop table if exists Iscritto;
 drop table if exists Fornitore;
 drop table if exists Fattura;
+drop table if exists Registrato;
+drop table if exists Certifica;
 
 drop trigger if exists Categoria_Scaglione;
 
@@ -57,7 +59,6 @@ Descrizione  TEXT,
 Quantita     SMALLINT DEFAULT 0 NOT NULL,
 Costo	     DECIMAL(5,2) DEFAULT 0 NOT NULL,
 PercentualeIVA	 INT(2) DEFAULT 0 NOT NULL,
-Foto		 MEDIUMBLOB,
 Categoria	 CHAR(20),
 
 PRIMARY KEY (CodProdotto),
@@ -76,44 +77,27 @@ Indirizzo    CHAR(50) NOT NULL,
 PRIMARY KEY (CodIscritto)
 ) ENGINE=InnoDB;
 
-CREATE TABLE Scontrino(
-Prodotto     INT,
-Data	     DATE,
-CodScontrino INT,
-Quantita     SMALLINT NOT NULL,
-SubTotale    DECIMAL(5,2) NOT NULL,
-Iscritto     INT,
+CREATE TABLE Scontrino (
+Id	INT AUTO_INCREMENT,
+Prodotto	INT,
+Data	DATE NOT NULL,
+CodScontrino	INT NOT NULL,
+Quantita	SMALLINT NOT NULL,
+SubTotale	DECIMAL(5,2) NOT NULL,
+Iscritto	INT,
 
-PRIMARY KEY (Prodotto,Data,CodScontrino)
-) ENGINE=InnoDB;
-
-CREATE INDEX IScontrino
-ON Scontrino (Prodotto);
-
-CREATE INDEX IScontrino2
-ON Scontrino (Data,CodScontrino);
-
-CREATE TABLE Certifica(
-Prodotto     INT,
-Data	     Date,
-CodScontrino    INT,
-
-PRIMARY KEY (Prodotto, Data, CodScontrino)
+PRIMARY KEY (Id),
+FOREIGN KEY (Iscritto) REFERENCES Iscritto(CodIscritto) ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
-CREATE INDEX ICertifica
-ON Certifica (Prodotto);
+CREATE TABLE Certifica (
+Prodotto	INT,
+Scontrino	INT,
 
-CREATE INDEX ICertifica2
-ON Certifica (Data,CodScontrino);
-
-ALTER TABLE Scontrino
-      ADD CONSTRAINT fkScontrino_Certifica FOREIGN KEY (Prodotto) REFERENCES Certifica (Prodotto),
-      ADD CONSTRAINT fkScontrino_Iscritto FOREIGN KEY (Iscritto) REFERENCES Iscritto (CodIscritto) ON DELETE CASCADE;
-
-ALTER TABLE Certifica
-      ADD CONSTRAINT fkCertifica_Prodotto FOREIGN KEY (Prodotto) REFERENCES Prodotto (CodProdotto) ON DELETE CASCADE,
-      ADD CONSTRAINT fkCertifica_Scontrino FOREIGN KEY (Data,CodScontrino) REFERENCES Scontrino (Data,CodScontrino) ON DELETE CASCADE;
+PRIMARY KEY (Prodotto, Scontrino),
+FOREIGN KEY (Prodotto) REFERENCES Prodotto (CodProdotto),
+FOREIGN KEY (Scontrino) REFERENCES Scontrino (Id)
+) ENGINE = InnoDB;
 
 CREATE TABLE Fornitore(
 Nome   CHAR(50),
@@ -126,38 +110,22 @@ PRIMARY KEY (Nome)
 ) ENGINE=InnoDB;
 
 CREATE TABLE Fattura(
-Prodotto     INT,
-CodFattura   INT,
-Data	     DATE NOT NULL,
-Quantita     SMALLINT NOT NULL,
-Fornitore    CHAR(50),
+Id	INT AUTO_INCREMENT,
+Prodotto	INT,
+CodFattura	INT NOT NULL,
+Data	DATE NOT NULL,
+Qauntita	SMALLINT NOT NULL,
+Fornitore	CHAR(50),
 
-PRIMARY KEY (Prodotto,CodFattura)
-) ENGINE = InnoDB;
-
-CREATE INDEX IFattura
-ON Fattura (Prodotto);
-
-CREATE INDEX IFattura2
-ON Fattura (CodFattura);
-
-CREATE TABLE Registrato(
-Prodotto     INT,
-Fattura	     INT,
-
-PRIMARY KEY (Prodotto,Fattura)
+PRIMARY KEY (Id),
+FOREIGN KEY (Fornitore) REFERENCES Fornitore (Nome)
 ) ENGINE=InnoDB;
 
-CREATE INDEX IRegistrato
-ON Registrato (Prodotto);
+CREATE TABLE Registrato(
+Prodotto	INT,
+Fattura		INT,
 
-CREATE INDEX IRegistrato2
-ON Registrato (Fattura);
-
-ALTER TABLE Fattura
-      ADD CONSTRAINT fkFattura_Registrato FOREIGN KEY (Prodotto) REFERENCES Registrato (Prodotto),
-      ADD CONSTRAINT fkFattura_Fornitore FOREIGN KEY (Fornitore) REFERENCES Fornitore (Nome) ON DELETE CASCADE; #da controllare
-
-ALTER TABLE Registrato
-      ADD CONSTRAINT fkRegistrato_Prodotto FOREIGN KEY (Prodotto) REFERENCES Prodotto (CodProdotto),
-      ADD CONSTRAINT fkRegistrato_Fattura FOREIGN KEY (Fattura) REFERENCES Fattura(CodFattura);
+PRIMARY KEY (Prodotto, Fattura),
+FOREIGN KEY (Prodotto) REFERENCES Prodotto (CodProdotto),
+FOREIGN KEY (Fattura) REFERENCES Fattura (Id)
+) ENGINE = InnoDB;
