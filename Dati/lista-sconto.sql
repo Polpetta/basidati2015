cc03d0545d7c159b2fc16724a237e8931ad5b7b6
Davide Polonio <poloniodavide@gmail.com>
2015-06-23 11:45:47 +0200

    Aggiunti dati per il DB

---
+++ Dati/lista-sconto.sql
@@ -0,0 +1,69 @@
+ INSERT INTO `dpolonio-PR`.`Sconto` (
+`Categoria` ,
+`Livello` ,
+`PercentualeSconto` ,
+`TettoMax`
+)
+VALUES (
+'Liste nozze', 0, 0, 10
+), (
+'Liste nozze', 1, 5, 15
+), (
+'Liste nozze', 2, 10, 20
+), (
+'Liste nozze', 3, 15, 25
+), (
+'Liste nozze', 4, 20, 30
+), (
+'Liste nozze', 5, 30, NULL
+), (
+'Porcellane', 0, 0, 10
+), (
+'Porcellane', 1, 5, 15
+), (
+'Porcellane', 2, 10, 20
+), (
+'Porcellane', 3, 15, 25
+), (
+'Porcellane', 4, 20, NULL
+), (
+'Pentolame', 0, 0, 10
+), (
+'Pentolame', 1, 5, 15
+), (
+'Pentolame', 2, 10, 20
+), (
+'Pentolame', 3, 15, 25
+), (
+'Pentolame', 4, 20, 30
+), (
+'Pentolame', 5, 30, NULL
+), (
+'Tovaglie', 0, 0, 10
+), (
+'Tovaglie', 1, 5, 15
+), (
+'Tovaglie', 2, 10, NULL
+), (
+'Tavola', 0, 0, 10
+), (
+'Tavola', 1, 5, 15
+), (
+'Tavola', 2, 10, 20
+), (
+'Tavola', 3, 15, 25
+), (
+'Tavola', 4, 20, NULL
+), (
+'Paralumi', 0, 0, 10
+), (
+'Paralumi', 1, 5, 15
+), (
+'Paralumi', 2, 10, 20
+), (
+'Paralumi', 3, 15, 25
+), (
+'Paralumi', 4, 20, 30
+), (
+'Paralumi', 5, 35, NULL
+);
