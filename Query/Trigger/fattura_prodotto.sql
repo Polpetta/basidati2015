b343f8069f23c95ba4b0c74107a71b00085284f9
BARI Alessandro [IN2] <abari@t32.torre.math.unipd.it>
2015-06-23 17:03:45 +0200

    Corretti trigger fattura_prodotto e scontrino_prodotti gg

--- Query/Trigger/fattura_prodotto.sql
+++ Query/Trigger/fattura_prodotto.sql
@@ -1,24 +1,26 @@
 DROP TRIGGER IF EXISTS increaseqta;
-
-DELIMITER $$
-
+DELIMITER // 
 CREATE TRIGGER increaseqta
 AFTER INSERT ON Fattura
 FOR EACH ROW
 BEGIN
-IF EXISTS (SELECT * FROM Prodotto WHERE Prodotto.CodProdotto=New.Prodotto)
-THEN
-	DECLARE oldqta int;
-	(SELECT P.Quantita INTO oldqta
-	FROM Prodotto AS P
-	WHERE P.CodProdotto=New.Prodotto);
-	
-	IF(oldqta=-1)
-		THEN set oldqta=0;
-	END IF;
-	(SET P.Quantita=oldqta+new.Quantita
-	WHERE New.Prodotto=Prodotto.CodProdotto);
-END IF;
-END$$
+DECLARE oldqta INT;
+DECLARE nuovo_prodotto INT;
+SELECT CodProdotto INTO nuovo_prodotto
+FROM Prodotto as P, Fattura as F, Registrato as R
+WHERE F.Id=New.Id &&
+	  F.Id=R.Fattura && 
+	  R.Prodotto=P.CodProdotto;
 
+SELECT P.Quantita INTO oldqta
+FROM Prodotto AS P
+WHERE P.CodProdotto=nuovo_prodotto;
+
+IF(oldqta=-1)
+	THEN set oldqta=0;
+END IF;
+SET P.Quantita=oldqta+new.Quantita
+WHERE nuovo_prodotto=Prodotto.CodProdotto;
+END IF;
+END//
 DELIMITER ;
