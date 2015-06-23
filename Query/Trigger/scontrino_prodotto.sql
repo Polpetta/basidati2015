04a4b86178dd952151a9db37db50cfbe0d3a0e48
BARI Alessandro [IN2] <abari@t32.torre.math.unipd.it>
2015-06-23 17:03:45 +0200

    Corretti trigger fattura_prodotto e scontrino_prodotti gg

--- Query/Trigger/scontrino_prodotto.sql
+++ Query/Trigger/scontrino_prodotto.sql
@@ -1,40 +1,33 @@
 DROP TRIGGER IF EXISTS scontrino_prodotto;
-
 DELIMITER //
 CREATE TRIGGER scontrino_prodotto
 BEFORE INSERT ON Scontrino
 FOR EACH ROW
 BEGIN
-
 DECLARE vecchia_qta SMALLINT;
-SELECT P.Quantita INTO vecchia_qta FROM Prodotto AS P WHERE P.CodProdotto=New.Prodotto;
+DECLARE nuovo_prodotto INT;
+DECLARE qta_necessaria SMALLINT;
+DECLARE Forn CHAR;	
+
+SELECT CodProdotto INTO nuovo_prodotto FROM Prodotto JOIN Scontrino ON Scontrino.CodScontrino=New.CodScontrino;
+SELECT Quantita INTO vecchia_qta FROM Prodotto AS P WHERE P.CodProdotto=nuovo_prodotto;
 
 IF vecchia_qta=-1
 	THEN SET vecchia_qta=0;
 END IF;
-
-IF vecchia_qta-New.quantita < 0
+IF ((vecchia_qta-New.quantita) < 0)
 THEN
-	DECLARE id_necessario INT;
-	SELECT MAX(Id) INTO id_necessario FROM Fattura;
-	id_necessario=id_necessario+1;
-	
-	DECLARE qta_necessaria SMALLINT;
-	qta_necessaria=vecchia_qta-New.Quantita*-1;
-	
-	DECLARE Forn CHAR;	
-	SELECT F.Fornitore INTO Forn FROM Fattura AS F WHERE F.Prodotto=New.Prodotto;
-	
-	INSERT INTO Fattura (Id,Prodotto,CodFattura,Data,Quantita,Fornitore)
-	VALUES (id_necessario,New.Prodotto,999,sysdate(),qta_necessaria,Forn);
-	
+	SET qta_necessaria=(vecchia_qta-New.Quantita)*(-1);
+	SELECT Fornitore INTO Forn FROM Fattura INNER JOIN Prodotto ON Prodotto=nuovo_prodotto;
+	INSERT INTO Fattura (Prodotto,CodFattura,Data,Quantita,Fornitore)
+	VALUES (nuovo_prodotto,999,sysdate(),qta_necessaria,Forn);
 	UPDATE Prodotto
-	SET Prodotto.Quantita=0 WHERE New.Prodotto=CodProdotto;
+	SET Prodotto.Quantita=0 WHERE nuovo_prodotto=CodProdotto;
 ELSE
 	UPDATE Prodotto
-	SET Prodotto.Quantita=vecchai_qta-New.quantita WHERE New.Prodotto=CodProdotto;
+	SET Prodotto.Quantita=vecchai_qta-New.quantita WHERE nuovo_prodotto=CodProdotto;
 END IF;
-END;//
-DELIMITER;
+END//
+DELIMITER ;
 
 
