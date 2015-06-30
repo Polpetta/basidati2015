2958038ef2f0e8c33a5636235978df264da17069
Davide Polonio <poloniodavide@gmail.com>
2015-06-30 17:12:35 +0200

    Procedura per aggiungere scontrino. Da completare

---
+++ Query/Procedure/Procedure_NuovoScontrino.sql
@@ -0,0 +1,24 @@
+DROP PROCEDURE IF EXISTS NuovoScontrino;
+
+DELIMITER $$
+
+CREATE PROCEDURE NuovoScontrino(SData DATE, SCodScontrino INT, SQuantita SMALLINT, SIscritto INT, CProdotto INT)
+BEGIN
+DECLARE SSubTotale DECIMAL(8,2);
+DECLARE UltimoScontrino INT;
+
+#bisogna resettare il numero dello scontrino quando cambia il giorno!
+
+SELECT Costo INTO SSubTotale FROM Prodotto WHERE CodProdotto = CProdotto;
+
+SET SSubTotale = SQuantita * SSubTotale;
+
+INSERT INTO Scontrino (Data,CodScontrino,Quantita,SubTotale,Iscritto) VALUES (SData,SCodScontrino,SQuantita,SSubTotale,SIscritto);
+
+SELECT MAX(Id) INTO UltimoScontrino FROM Sconto;
+
+INSERT INTO Certifica VALUES (CProdotto,UltimoScontrino);
+
+
+END$$
+DELIMITER ;
