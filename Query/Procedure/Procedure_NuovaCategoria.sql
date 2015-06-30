a37db7e7ad8277225f4ee2364661c6e51121f048
Davide Polonio <poloniodavide@gmail.com>
2015-06-30 15:45:31 +0200

    Nuova procedura per aggiungere Categorie. -La procedura aggiunge un rispettivo dipendente per ogni nuova categoria e anche un livello di sconto di base.

---
+++ Query/Procedure/Procedure_NuovaCategoria.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS NuovaCategoria;
+DELIMITER $$
+
+CREATE PROCEDURE NuovaCategoria(NCategoria CHAR(20), NDip CHAR(15), CDip CHAR(15), DData DATE, DCodF CHAR(16), DTel CHAR(10), DMail CHAR(50), DInd CHAR(50), DPswd CHAR(64), SLvl SMALLINT, PrcSconto INT(2), STettoMax SMALLINT)
+BEGIN
+DECLARE UltimaCategoria INT;
+
+
+INSERT INTO Sconto (Livello, PercSconto,TettoMax) VALUES (SLvl,PrcSconto,STettoMax);
+
+INSERT INTO Categoria VALUES (NCategoria);
+
+SELECT MAX(Id) INTO UltimaCategoria FROM Sconto;
+
+INSERT INTO Scaglione VALUES (NCategoria, UltimaCategoria); 
+
+
+INSERT INTO Dipendente (Nome,Cognome,DataNascita,CodFiscale,Telefono,Mail,DataInizio,Indirizzo,Categoria,Password) VALUES (NDip,CDip,DData,DCodF,DTel,DMail,NOW(),DInd,NCategoria,DPswd);
+
+END$$
+DELIMITER ;
