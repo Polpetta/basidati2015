920e130eafc3a51e08b788f92d1c894f1b3e91d8
Davide Polonio <poloniodavide@gmail.com>
2015-07-01 16:31:23 +0200

    Aggiunta procedura NuovoScontrino -Crea uno scontrino solo se ci sono abbastanza prodotti.

--- Query/Procedure/Procedure_NuovoScontrino.sql
+++ Query/Procedure/Procedure_NuovoScontrino.sql
@@ -6,17 +6,29 @@ CREATE PROCEDURE NuovoScontrino(SData DATE, SCodScontrino INT, SQuantita SMALLIN
 BEGIN
 DECLARE SSubTotale DECIMAL(8,2);
 DECLARE UltimoScontrino INT;
+DECLARE QtaPrd INT;
 
+SELECT Quantita INTO QtaPrd FROM Prodotto WHERE CodProdotto = CProdotto;
+
+IF (QtaPrd != -1 AND SQuantita <= QtaPrd)
+THEN
 SELECT Costo INTO SSubTotale FROM Prodotto WHERE CodProdotto = CProdotto;
 
 SET SSubTotale = SQuantita * SSubTotale;
 
 INSERT INTO Scontrino (Data,CodScontrino,Quantita,SubTotale,Iscritto) VALUES (SData,SCodScontrino,SQuantita,SSubTotale,SIscritto);
 
-SELECT MAX(Id) INTO UltimoScontrino FROM Sconto;
+SELECT MAX(Id) INTO UltimoScontrino FROM Scontrino;
 
 INSERT INTO Certifica VALUES (CProdotto,UltimoScontrino);
 
+
+UPDATE Prodotto SET Quantita=Quantita-SQuantita WHERE CodProdotto=CProdotto;
+
+ELSE
+INSERT INTO Prodotto SELECT * FROM Prodotto LIMIT 1;
+
+END IF;
 
 END$$
 DELIMITER ;
