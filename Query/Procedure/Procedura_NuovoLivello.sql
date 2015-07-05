6f71a339ac0f698d3fcb51aac08bedfab7d1a27a
ale113 <alessbari94@gmail.com>
2015-07-05 17:55:29 +0200

    introdotto controllo sulla scalarità di TettoMax e percSconto

--- Query/Procedure/Procedura_NuovoLivello.sql
+++ Query/Procedure/Procedura_NuovoLivello.sql
@@ -7,18 +7,29 @@ DECLARE IdSconto INT;
 
 IF EXISTS(SELECT* FROM Categoria C WHERE C.NomeCategoria=NCategoria)
 THEN
-	IF NOT EXISTS(SELECT * 
-				  FROM Sconto SC, Scaglione SCA,Categoria C
-				  WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello=NLivello)
-				  THEN
-						INSERT INTO Sconto(Livello, PercSconto, TettoMax) VALUES (NLivello,NPercSconto,NTettoMax);
-						SELECT Max(Id) INTO IdSconto FROM Sconto;
-						INSERT INTO Scaglione(Categoria,Sconto) VALUES(NCategoria,IdSconto);
-				  ELSE
-						INSERT INTO Sconto SELECT * FROM SCONTO LIMIT 1;
-				  END IF;
+	IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
+	WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello=NLivello)
+	THEN
+		IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
+		WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.TettoMax >= NTettoMax)
+		THEN
+			IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
+			WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello<NLivello AND SC.PercSconto >= NPercSconto)
+			THEN
+				INSERT INTO Sconto(Livello, PercSconto, TettoMax) VALUES (NLivello,NPercSconto,NTettoMax);
+				SELECT Max(Id) INTO IdSconto FROM Sconto;
+				INSERT INTO Scaglione(Categoria,Sconto) VALUES(NCategoria,IdSconto);
+			ELSE
+				INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
+			END IF;
+		ELSE
+			INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
+		END IF;
+	ELSE
+		INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
+	END IF;
 ELSE 
-	INSERT INTO Sconto SELECT * FROM SCONTO LIMIT 1;
+	INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
 END IF;
 END ||
 DELIMITER ;
