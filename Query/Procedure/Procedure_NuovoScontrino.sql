a5cf9dc653a324186d8c90c417c966e507cd377a
ale113 <alessbari94@gmail.com>
2015-07-05 11:24:04 +0200

    Inserito calcolo sconto in base al livello acquisti e modifica del campo subtotale

--- Query/Procedure/Procedure_NuovoScontrino.sql
+++ Query/Procedure/Procedure_NuovoScontrino.sql
@@ -9,29 +9,41 @@ DECLARE UltimoScontrino INT;
 DECLARE QtaPrd INT;
 DECLARE Iva INT;
 DECLARE Piva INT;
+DECLARE Psconto INT;
+DECLARE Scont INT;
+DECLARE Cat CHAR(50);
 
 SELECT Quantita INTO QtaPrd FROM Prodotto WHERE CodProdotto = CProdotto;
 
 IF (QtaPrd != -1 AND SQuantita <= QtaPrd)
 THEN
-SELECT Costo INTO SSubTotale FROM Prodotto WHERE CodProdotto = CProdotto;
+	SELECT Costo INTO SSubTotale FROM Prodotto WHERE CodProdotto = CProdotto;
 
-SET SSubTotale = SQuantita * SSubTotale;
-SELECT PercentualeIVA INTO Piva FROM Prodotto WHERE CodProdotto=CProdotto;
-SET Iva=(SSubTotale*Piva)/100;
-SET SSubTotale= SSubTotale+Iva;
+	SET SSubTotale = SQuantita * SSubTotale;
+	SELECT PercentualeIVA INTO Piva FROM Prodotto WHERE CodProdotto=CProdotto;
+	
+	SET Iva=(SSubTotale*Piva)/100;
+	SET SSubTotale= SSubTotale+Iva;
 
-INSERT INTO Scontrino (Data,CodScontrino,Quantita,SubTotale,Iscritto) VALUES (SData,SCodScontrino,SQuantita,SSubTotale,SIscritto);
+	INSERT INTO Scontrino (Data,CodScontrino,Quantita,SubTotale,Iscritto) VALUES (SData,SCodScontrino,SQuantita,SSubTotale,SIscritto);
 
-SELECT MAX(Id) INTO UltimoScontrino FROM Scontrino;
+	SELECT MAX(Id) INTO UltimoScontrino FROM Scontrino;
 
-INSERT INTO Certifica VALUES (CProdotto,UltimoScontrino);
+	INSERT INTO Certifica VALUES (CProdotto,UltimoScontrino);
 
-
-UPDATE Prodotto SET Quantita=Quantita-SQuantita WHERE CodProdotto=CProdotto;
-
+	SELECT P.Categoria INTO Cat FROM Prodotto P WHERE P.CodProdotto=CProdotto;
+	
+	UPDATE Prodotto SET Quantita=Quantita-SQuantita WHERE CodProdotto=CProdotto;
+	
+	SELECT max(SC.PercSconto) INTO Psconto
+	FROM Scontrino S,Categoria C,Sconto SC,Iscritto I, Certifica CE, Prodotto P,Scaglione SCA
+	WHERE S.Iscritto=I.CodIscritto AND CE.Scontrino=S.Id AND CE.Prodotto=P.CodProdotto AND P.Categoria=C.NomeCategoria AND SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id
+	AND I.CodIscritto=SIscritto AND C.NomeCategoria=Cat;
+	
+	SET Scont=(SSubTotale*Psconto)/100;
+	UPDATE Scontrino SET SubTotale=SSubTotale-Scont WHERE Scontrino.Id=UltimoScontrino;
 ELSE
-INSERT INTO Prodotto SELECT * FROM Prodotto LIMIT 1;
+	INSERT INTO Prodotto SELECT * FROM Prodotto LIMIT 1;
 
 END IF;
 
