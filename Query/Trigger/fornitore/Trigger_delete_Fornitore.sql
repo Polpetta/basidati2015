3598f2abbca2fabdcccf0022751b92cc3053b391
polpetta <poloniodavide@gmail.com>
2015-07-06 17:15:27 +0200

    Aggiunti nuovi trigger per la gestione delle cancellazioni.

---
+++ Query/Trigger/fornitore/Trigger_delete_Fornitore.sql
@@ -0,0 +1,14 @@
+DROP TRIGGER IF EXISTS delete_fornitore;
+
+DELIMITER $$
+CREATE TRIGGER delete_fornitore
+BEFORE DELETE ON Fornitore
+FOR EACH ROW
+BEGIN
+
+DELETE FROM Registrato WHERE Fattura = ANY (SELECT F.Id FROM Fattura F WHERE F.Fornitore = old.Nome);
+
+
+END $$
+DELIMITER ;
+
