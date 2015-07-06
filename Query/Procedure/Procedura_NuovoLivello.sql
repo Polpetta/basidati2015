8c1ec3fa4b23c0b4087e386af02548f6f8b5d894
Alessandro Bari <alessbari94@gmail.com>
2015-07-06 10:48:51 +0200

    aggiunto controllo su percentuale sconto

--- Query/Procedure/Procedura_NuovoLivello.sql
+++ Query/Procedure/Procedura_NuovoLivello.sql
@@ -5,31 +5,34 @@ CREATE PROCEDURE NuovoLivello(NLivello SMALLINT,NPercSconto INT(2), NTettoMax	SM
 BEGIN
 DECLARE IdSconto INT;
 
-IF EXISTS(SELECT* FROM Categoria C WHERE C.NomeCategoria=NCategoria)
+IF (NPercSconto >=0)
 THEN
-	IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
-	WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello=NLivello)
+	IF EXISTS(SELECT* FROM Categoria C WHERE C.NomeCategoria=NCategoria)
 	THEN
 		IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
-		WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.TettoMax >= NTettoMax)
+		WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello=NLivello)
 		THEN
 			IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
-			WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello<NLivello AND SC.PercSconto >= NPercSconto)
+			WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.TettoMax >= NTettoMax)
 			THEN
-				INSERT INTO Sconto(Livello, PercSconto, TettoMax) VALUES (NLivello,NPercSconto,NTettoMax);
-				SELECT Max(Id) INTO IdSconto FROM Sconto;
-				INSERT INTO Scaglione(Categoria,Sconto) VALUES(NCategoria,IdSconto);
+				IF NOT EXISTS(SELECT * FROM Sconto SC, Scaglione SCA,Categoria C
+				WHERE SCA.Categoria=C.NomeCategoria AND SCA.Sconto=SC.Id AND C.NomeCategoria=NCategoria AND SC.Livello<NLivello AND SC.PercSconto >= NPercSconto)
+				THEN
+					INSERT INTO Sconto(Livello, PercSconto, TettoMax) VALUES (NLivello,NPercSconto,NTettoMax);
+					SELECT Max(Id) INTO IdSconto FROM Sconto;
+					INSERT INTO Scaglione(Categoria,Sconto) VALUES(NCategoria,IdSconto);
+				ELSE
+					INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
+				END IF;
 			ELSE
 				INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
 			END IF;
 		ELSE
 			INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
 		END IF;
-	ELSE
+	ELSE 
 		INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
 	END IF;
-ELSE 
-	INSERT INTO Sconto SELECT * FROM Sconto LIMIT 1;
 END IF;
 END ||
 DELIMITER ;
